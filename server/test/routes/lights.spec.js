e634c1b6a1b7ecdfc9facb6ae1e97b6bdad25c94
Albert J. Wong <awong.dev@gmail.com>
2016-12-22 00:35:49 -0800

    Set up testing framework.

---
+++ server/test/routes/lights.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const assert = require(`assert`);
+const lights = require(`../../routes/lights`);
+
+describe('lights route', () => {
+  it(`should create an express app`, (done) => {
+    done()
+  });
+});
