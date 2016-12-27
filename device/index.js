6af286fa0eda212211f717cedff25fbec277e9a4
Albert J. Wong <awong.dev@gmail.com>
2016-12-27 00:54:23 -0800

    Remove color cause syslog doesn't like it

--- device/index.js
+++ device/index.js
@@ -6,13 +6,18 @@ const deviceName = process.env.DEVICE_NAME || 'test light';
 const devicePassword = process.env.DEVICE_PASSWORD;
 const iotzServer = process.env.IOTZ_SERVER || 'http://localhost:8080';
 
-// GPIO16 (is pin 36) which defaults to pull-down. Set it that way anyways
-// just to be certain.  Low is off on the relay.
-const lightControlGpio = 16;
-const gpioRoot = '/sys/class/gpio';
-const gpioLightControlPath = `${gpioRoot}/gpio${lightControlGpio}`;
+// GPIO16 (physical pin 36) defaults to pull-down. Low is off on the relay
+// so this is a good default.
+const lightPowerGpio = 16;
 
-const fs = require('fs');
+// GPIO3 (physical pin 5) defaults to pull-up. Having the switch sourced
+// to ground felt better...no real reason otherwise for the choice. It means
+// interrupt should trigger on falling edge and expect false on read for
+// debounce.
+const lightSwitchGpio = 3;
+
+const async = require('async');
+const gpio = require('rpi-gpio');
 const request = require('request');
 const winston = require('winston');
 
@@ -22,46 +27,79 @@ const logger = new (winston.Logger)({
       level:            'info',
       handleExceptions: true,
       json:             false,
-      colorize:         true,
-      timestamp: true
+      colorize:         false,
+      timestamp: false
     })
   ]
 });
 
-function writeValue(path, value) {
-  const fd = fs.openSync(path, "w");
-  fs.writeSync(fd, value);
-  fs.closeSync(fd);
+function listenForSwitch(state) {
+  // Interrupts are expressed as 'change' events from Gpio using the
+  // EventEmitter interface. Use `once` to implement software debounce.
+  // Using 10ms for debounce seems to provide good protection and
+  // responsiveness.
+  gpio.once('change', () => {
+      setTimeout(() => {
+        // Actually attempt to read the input value after 10 secs
+        // and then reschedule the listener.
+        async.waterfall([
+            (cb) => {
+              gpio.read(lightSwitchGpio, (err, value) => {
+                cb(err, value === false);
+              });
+            },
+            (shouldToggle, cb) => toggleLight(state, shouldToggle, cb)
+          ],
+          (err, value) => {
+            if (err) {
+              logger.error(JSON.stringify(err));
+            }
+            listenForSwitch(state);
+          });
+      }, 10);
+    });
 }
 
-function setupGpio() {
-  if (!fs.existsSync(gpioLightControlPath)) {
-    writeValue(`${gpioRoot}/export`, `${lightControlGpio}`);
+function setupGpio(done) {
+  gpio.setMode(gpio.MODE_BCM);
+  async.series([
+      (cb) => gpio.setup(lightPowerGpio, gpio.DIR_OUT, cb),
+      (cb) => gpio.setup(lightSwitchGpio, gpio.DIR_IN, gpio.EDGE_FALLING, cb),
+    ],
+    (err) => done(err)
+    );
+}
+
+function updateLight(isOn, cb) {
+  gpio.write(lightPowerGpio, isOn ? '1' : '0', (err) => {
+    return cb(err);
+  });
+}
+
+function toggleLight(state, shouldToggle, cb) {
+  if (shouldToggle) {
+    Object.assign(state, {
+        isOn: !state.isOn,
+        deviceSequence: state.deviceSequence + 1
+      });
+    async.waterfall([
+        (cb) => updateLight(state.isOn, cb),
+        (cb) => sendHeartbeat(state, cb)
+      ],
+      (err) => cb(err));
+  } else {
+    cb(null);
   }
-  writeValue(`${gpioLightControlPath}/direction`, 'out');
 }
 
-// TODO(awong): You have a race here. Careful. Another updateLight operation can start before this is done.
-function updateLight(isOn) {
-  writeValue(`${gpioLightControlPath}/value`, isOn ? '1' : '0');
-}
-
-function toggleLight(state) {
-  // Talk to GPIO here.
-  lightState.isOn = !lightState.isOn;
-}
-
-// TODO(awong): Pass lightState in via parameter.
-function processStatusUpdate(err, resp, body, prevState) {
+function processStatusUpdate(err, resp, body, prevState, cb) {
   if (err) {
-    logger.error(err);
-    return;
+    return cb(err);
   }
 
   logger.info(`Heatbeat response: [${resp.statusCode}] ${JSON.stringify(body)}`);
   if (resp.statusCode !== 200) {
-    logger.error(`Bad Status: ${resp.statusCode}`);
-    return;
+    return cb(`Bad Status: ${resp.statusCode}`);
   }
 
   // Only override out on state if the server shows that it was aware of our
@@ -71,14 +109,13 @@ function processStatusUpdate(err, resp, body, prevState) {
     const newState = Object.assign({}, prevState);
     newState.isOn = body.isOn;
     logger.info(`Transitioning state from ${prevState.isOn} to ${newState.isOn}`);
-    updateLight(newState.isOn);
-    return newState;
+    return updateLight(newState.isOn, (err) => { cb(err, newState); });
   }
 
-  return prevState;
+  return cb(null, prevState);
 }
 
-function sendHeartbeat(state) {
+function sendHeartbeat(state, done) {
   request.post( {
     url: `${iotzServer}/api/lights/device/${deviceId}`,
     method: 'POST',
@@ -90,7 +127,13 @@ function sendHeartbeat(state) {
     }
   },
   (err, resp, body) => {
-    Object.assign(state, processStatusUpdate(err, resp, body, state));
+    processStatusUpdate(err, resp, body, state, (err, newState) => {
+        if (err) {
+          return done(err);
+        }
+        Object.assign(state, newState);
+        done(null);
+      });
   });
 }
 
@@ -98,15 +141,25 @@ function sendHeartbeat(state) {
 if (module === require.main) {
   const lightState = { name: deviceName, isOn: false, deviceSequence: 0 };
   const heartbeatPeriod = 5000;
-  setupGpio();
-  updateLight(lightState.isOn);
-  sendHeartbeat(lightState);
+  async.series([
+      setupGpio,
+      (cb) => updateLight(lightState.isOn, cb),
+    ],
+    (err) => {
+      if (err) {
+        logger.error(JSON.stringify(err));
+      }
+    });
   const startHeartbeat = () => {
-    sendHeartbeat(lightState);
-
-    // Jittered heartbeat.
-    setTimeout(startHeartbeat, (heartbeatPeriod / 2) + (Math.random() * heartbeatPeriod));
+    sendHeartbeat(lightState, (err) => {
+        // Jittered heartbeat.
+        if (err) {
+          console.error(JSON.stringify(err));
+        }
+        setTimeout(startHeartbeat, (heartbeatPeriod / 2) + (Math.random() * heartbeatPeriod));
+      });
   };
+  listenForSwitch(lightState);
   startHeartbeat();
   logger.info(`Heatbeat started. Period: ${heartbeatPeriod}`);
 }
