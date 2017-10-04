d6daa9e93f7b141851c451393ff4072b14a165ee
Albert J. Wong <awong.dev@gmail.com>
2017-10-03 23:48:25 -0700

    Mostly working cloud function with auth.

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,47 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
 const bodyParser = require('body-parser');
+const pick = require('lodash/pick');
 
 const app = express();
 app.use(bodyParser.json());
 
 admin.initializeApp(functions.config().firebase);
+
+function isValidDeviceId(str) {
+  return /^(0|[1-9]\d*)$/.test(str);
+}
+
+// Authentication middlewear. Expects an OAuth2 token that can be decided by firebase.
+// Allowed Auth providers handled via the firebase configuraiton portal.
+// After this middlewear, |req| will have a |user| and an |access_type| field that
+// can be used for identity and authorization.
+function authenticate(req, res, next) {
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+  const idToken = req.headers.authorization.split('Bearer ')[1];
+  admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+    req.user = decodedIdToken;
+    const user_info = pick(req.user, ['email', 'email_verified', 'name']);
+    user_info.sign_in_provider = req.user.firebase.sign_in_provider
+    admin.database().ref(`/users/${req.user.sub}/info`).set(user_info);
+    admin.database().ref(`/users/${req.user.sub}/access_type`).once('value').then(snapshot => {
+	 if (['user', 'device'].includes(snapshot.val())) {
+	   req.access_type = snapshot.val();
+	   next();
+	 } else {
+	   res.status(403).send('Unauthorized');
+	 }
+    });
+  }).catch(error => {
+    console.error('Unable to authenticate', error.message);
+    res.status(403).send('Unauthorized');
+  });
+}
+
+app.use(authenticate);
 
 // Put allows for creation or updating of the device.
 // Required parameter is isOn (how to handle recirc pump?).
@@ -15,14 +51,17 @@ admin.initializeApp(functions.config().firebase);
 // updates to the name, deviceSequence, and heartbeat parameter.
 // TODO(awong): Heartbeat should not be required if we can detect FCM clients.
 //
-app.put('/devices/:deviceId', (req, res) => {
+// Request https://www.googleapis.com/auth/userinfo.email scope.
+app.put('/api/devices/:deviceId', (req, res) => {
   const deviceId = req.params.deviceId;
-  const isPutFromActualDevice = req.body.isDevice;
-  console.error(`Coming from: ${isPutFromActualDevice} ${JSON.stringify(req.params)} ${JSON.stringify(req.body)}`);
-  // TODO Validate input.
+  // TODO Validate other input.
+  if (!isValidDeviceId(deviceId)) {
+    res.status(400).json({errorCode: 400, errorMessage: `Invalid device Id '${deviceId}'`});
+    return;
+  }
   admin.database().ref(`/devices/${deviceId}`).transaction(
     (deviceState) => {
-      if (isPutFromActualDevice) {
+      if (req.access_type == 'device') {
         deviceState = {
           'name': req.body.name,
           'lastHeartbeat': new Date(),
@@ -52,19 +91,20 @@ app.put('/devices/:deviceId', (req, res) => {
 });
 
 // Retrieves the current status.
-app.get('/devices/:deviceId', (req, res) => {
+app.get('/api/devices/:deviceId', (req, res) => {
   const deviceId = req.params.deviceId;
-  // TODO Validate input.
+  if (!isValidDeviceId(deviceId)) {
+    res.status(400).json({errorCode: 400, errorMessage: `Invalid device Id '${deviceId}'`});
+    return;
+  }
   admin.database().ref(`/devices/${deviceId}`).once('value').then(snapshot => {
     if (snapshot.val() !== null) {
-      // Cache details in the browser for 5 minutes
-      res.set('Cache-Control', 'private, max-age=300');
       res.status(200).json(snapshot.val());
     } else {
       res.status(404).json({errorCode: 404, errorMessage: `device '${deviceId}' not found`});
     }
   }).catch(error => {
-    console.log('Error getting state for deviceId', deviceId, error.message);
+    console.error('Error getting state for deviceId', deviceId, error.message);
     res.sendStatus(500);
   });
 });
